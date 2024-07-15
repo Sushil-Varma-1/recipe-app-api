4b5d504a2844ca22ec993e7c1c92fbd110775635
Sushil Varma <svarmacompdse2021@bharatedu.co.in>
2024-07-15 17:52:51 +0530

    updated requirements.txt

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
