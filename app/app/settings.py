7e57c56403038276055a2036b4c97a7e8817f0ba
Sushil Varma <svarmacompdse2021@bharatedu.co.in>
2024-07-16 14:23:04 +0530

    Added swaggerui for api documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'core.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
